17932176799efe6dd8f08e5324618edee597c3f7
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-11-25 02:12:02 +0530

    added preview config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    // other config...
+  preview: {
+    host: true, // makes vite preview listen on 0.0.0.0
+    port: Number(process.env.PORT) || 4173, // Railway provides a port; fallback to 4173 locally
+  },
 })
